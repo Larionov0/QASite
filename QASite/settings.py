b93d5b572542b790a0503f659a7de17332734b6a
Larko <larionov1001@gmail.com>
2022-09-23 23:17:57 +0300

    Sign in + logout

--- QASite/settings.py
+++ QASite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!v(evp6x1k(m7m-a69f@h_by^)a5c8j-d9_d6f9#)9byo)!hqb
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+LOGIN_URL = "/auth/sign_in/"
 
 # Application definition
 
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MainApp'
+    'MainApp',
+    'Authsys'
 ]
 
 MIDDLEWARE = [
@@ -86,18 +87,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
